44e9373228c324e7c60b960160d50d1f0c6ba93a
Miguel Tavarez <tavarezster@gmail.com>
2017-05-22 16:22:58 +0000

    form_for lab for songs/artists/genres domain

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,28 @@
 class SongsController < ApplicationController
+    def index
+        @song = Song.all
+    end
+    
+    def show
+        @song = Song.find(params[:id])
+    end
+    
+    def new
+        @song = Song.new 
+    end
+    
+    def edit
+        @song = Song.find(params[:id])
+    end
+    
+    def create
+        @song = Song.create(name: params[:song][:name], genre_id: params[:song][:genre_id], artist_id: params[:song][:artist_id])
+        redirect_to @song
+    end
+    
+    def update
+        @song = Song.find(params[:id])
+        @song.update(name: params[:song][:name], genre_id: params[:song][:genre_id], artist_id: params[:song][:artist_id])
+        redirect_to @song
+    end
 end
